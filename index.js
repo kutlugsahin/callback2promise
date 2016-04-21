531b5e4180ac157ac39eb23a9d1dab23e42199a8
ahmetkutlu <ku>
2016-04-21 23:37:55 +0300

    refactored

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ module.exports = function (f) {
     var ctx = this;
     return function () {
         var fArgs = Array.prototype.slice.call(arguments);
+        var paramLength = f.length;
+        var args = [];
+
+        for (var i = 0; i < paramLength -1; i++) {
+            if(i < fArgs.length){
+                args.push(fArgs[i])
+            }else{
+                args.push(undefined);
+            }
+        }
 
         return new Promise((res, rej) => {
-            fArgs.push(function (err, result) {
+            args.push(function (err, result) {
                 if (err) rej(err);
                 else res(result);
             });
 
-            f.apply(ctx, fArgs);
+            f.apply(ctx, args);
         });
     }
 };

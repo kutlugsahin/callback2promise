be2ba6a98b3fc310261cedfeebddc7e65b69f5b3
ahmetkutlu <ku>
2016-04-21 23:12:51 +0300

    refactored

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ module.exports = function (f) {
     return function () {
         var fArgs = Array.prototype.slice.call(arguments);
 
-        if(fArgs[fArgs.length-1] instanceof Function){
-            fArgs.pop();
-        }
-
         return new Promise((res, rej) => {
             fArgs.push(function (err, result) {
                 if (err) rej(err);
